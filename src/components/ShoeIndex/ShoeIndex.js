2d4b57f50a34614ea0311ad3821161b5f6776799
Yury Shelest <13shell@mail.ru>
2022-08-29 22:22:10 +0000

    Workshop Responsive

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,71 +1,40 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS } from "../../constants";
 
-import Breadcrumbs from '../Breadcrumbs';
-import Select from '../Select';
-import Spacer from '../Spacer';
-import ShoeSidebar from '../ShoeSidebar';
-import ShoeGrid from '../ShoeGrid';
-
-const ShoeIndex = ({ sortId, setSortId }) => {
+const Sidebar = () => {
   return (
     <Wrapper>
-      <MainColumn>
-        <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
-        </Header>
-        <Spacer size={32} />
-        <ShoeGrid />
-      </MainColumn>
-      <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
-      </LeftColumn>
+      <Link href="/lifestyle">Lifestyle</Link>
+      <Link href="/jordan">Jordan</Link>
+      <ActiveLink href="/running">Running</ActiveLink>
+      <Link href="/basketball">Basketball</Link>
+      <Link href="/training">Training &amp; Gym</Link>
+      <Link href="/football">Football</Link>
+      <Link href="/skateboarding">Skateboarding</Link>
+      <Link href="/us-football">American Football</Link>
+      <Link href="/baseball">Baseball</Link>
+      <Link href="/golf">Golf</Link>
+      <Link href="/tennis">Tennis</Link>
+      <Link href="/athletics">Athletics</Link>
+      <Link href="/walking">Walking</Link>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: row-reverse;
-  align-items: baseline;
-  gap: 32px;
-`;
+const Wrapper = styled.aside``;
 
-const LeftColumn = styled.div`
-  flex-basis: 248px;
-`;
-
-const MainColumn = styled.div`
-  flex: 1;
-`;
-
-const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: baseline;
-`;
-
-const Title = styled.h2`
-  font-size: 1.5rem;
+const Link = styled.a`
+  display: block;
+  text-decoration: none;
   font-weight: ${WEIGHTS.medium};
+  color: var(--color-gray-900);
+  line-height: 2;
 `;
 
-export default ShoeIndex;
+const ActiveLink = styled(Link)`
+  color: var(--color-primary);
+`;
+
+export default Sidebar;
